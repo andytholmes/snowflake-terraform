006af0d6a7a1038a438eb6e6254124a12375fc45
andytholmes <andy@andytholmes.co.uk>
2023-05-02 12:29:37 +0100

    simple snowflake create db and warehouse

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # snowflake-terraform
  
+Aim to create a terraform project that provides examples of what can be done with terraform on snowflake
+
---
+++ main.tf
@@ -0,0 +1,60 @@
+# Configure the AzureRM provider
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      #version = "0.1.0" # You can use a different version if needed
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      #version = "~> 2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+# Retrieve Snowflake credentials from Azure Key Vault
+data "azurerm_key_vault" "example" {
+  name                = "kv-atholmes"
+  resource_group_name = "rg-ah_keyvault"
+}
+
+data "azurerm_key_vault_secret" "snowflake_password" {
+  name         = "snowflake-password"
+  key_vault_id = data.azurerm_key_vault.example.id
+}
+
+# Configure the Snowflake provider using the retrieved secrets
+provider "snowflake" {
+  account  = "noefgcv-jv58472"
+  username = "andy"
+  password = data.azurerm_key_vault_secret.snowflake_password.value
+  #region   = "eu-west-2" #"uk-south" # e.g., "us-west-2"
+}
+
+# Create a Snowflake database
+resource "snowflake_database" "example" {
+  name = "example_db"
+  comment = "Example database created by Terraform"
+}
+
+# Create a Snowflake warehouse
+resource "snowflake_warehouse" "example" {
+  name = "example_wh"
+  warehouse_size = "X-SMALL"
+  auto_suspend = 60
+  auto_resume = true
+  initially_suspended = true
+  comment = "Example warehouse created by Terraform"
+}
+
+# Output the database and warehouse names
+output "database_name" {
+  value = snowflake_database.example.name
+}
+
+output "warehouse_name" {
+  value = snowflake_warehouse.example.name
+}
